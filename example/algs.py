54f72e1a8fb3455508dcf2bc631cc58ae9c29bb7
jeanmico <costellojeanm@gmail.com>
2018-01-26 18:53:39 -0800

    track conditionals and assignments

--- example/algs.py
+++ example/algs.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+#global variables used for counting quicksort
+qcond = 0
+qasgn = 0 
+
+
 def validate(x, numeric_only=False):
     """
     performs basic checks on input
@@ -32,26 +37,37 @@ def bubblesort(x):
     For an array of n elements:
         pass through the array n times
         on each pass, swap adjacent values that are out of order
+    returns tuple of sorted array, # conditionals, # assignments
     """
+    cond = 0
+    asgn = 0
+
     if len(x) == 0:
         return x
+
     validate(x)
     for i in range(len(x)):
         for j in range(len(x)-i-1):
+            cond += 1
             if x[j] > x[j+1]:
+                asgn += 2
                 x[j], x[j+1] = x[j+1], x[j]
-    return x
+    return (x, cond, asgn)
 
 def partition(x, l, h):
     """
     finds a pivot point
     reorders list by comparing each element to the pivot
     """
+    global qasgn
+    qasgn += 1
     p = l
     for j in range(l + 1, h + 1):
         if x[j] < x[l]:
+            qasgn += 3
             p += 1
             x[j], x[p] = x[p], x[j]
+    qasgn += 2
     x[p], x[l] = x[l], x[p]
     return p
 
@@ -60,17 +76,30 @@ def qsort(x, low, high):
     calls partition function to break list into two parts
     recursively calls itself to sort each part
     """
+    global qcond
+    qcond += 1
     if low < high:
+        qcond += high - low
         p = partition(x, low, high)
         qsort(x, low, p - 1)
         qsort(x, p + 1, high)
 
 def quicksort(x):
     """
-    Describe how you are sorting `x`
+    calls qsort to perform quicksort algorithm
+    quicksort cannot handle strings, all elements must be numeric
+    uses global variables to count conditionals, assignments
+    returns tuple of sorted list, # conditionals, # assignments
     """
+    global qcond
+    qcond = 0
+
+    global qasgn
+    qasgn = 0
+
     if len(x) == 0:
         return x
     validate(x, True)
+
     qsort(x, 0, len(x) -1)
-    return x
+    return (x, qcond, qasgn)
